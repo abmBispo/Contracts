6e80d44a315674ca411c96a268717261e4ea6e83
Alan Borges <sr.alan.bispo@gmail.com>
2020-08-19 11:43:25 -0300

    Updates in every tab change

--- contracts-frontend/src/components/Contract/List/index.js
+++ contracts-frontend/src/components/Contract/List/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table } from 'antd';
 import axios from 'axios';
 import { BASE_URL } from '../../../main/consts';
@@ -31,58 +31,44 @@ const mapApiToProps = params => {
   };
 };
 
-export default class List extends React.Component {
-  state = {
-    data: [],
-    pagination: {
-      current: 1,
-      pageSize: 25,
-    },
-    loading: false,
-  };
+export default (props) => {
+  const [loading, setLoading] = useState(false);
+  const [pagination, setPagination] = useState({ current: 1, pageSize: 25 });
+  const [data, setData] = useState([]);
 
-  componentDidMount() {
-    const { pagination } = this.state;
-    this.fetch({ pagination });
+  useEffect(() => fetch(), [props.activeKey]);
+
+  const fetch = (params = {}) => {
+    setLoading(true);
+    axios
+      .get(`${BASE_URL}/contracts?page=${pagination.current}`)
+      .then((res) => {
+        const { data } = mapApiToProps(res.data);
+        setLoading(false);
+        setData(data);
+        setPagination({
+          ...pagination,
+          ...res.data.pagination
+        });
+      });
   }
 
-  handleTableChange = (pagination, filters, sorter) => {
-    this.fetch({
+  const handleTableChange = (pagination, filters, sorter) => {
+    fetch({
       sortField: sorter.field,
       sortOrder: sorter.order,
       pagination
     });
-  };
-
-  fetch = (params = {}) => {
-    this.setState({ loading: true });
-    axios
-      .get(`${BASE_URL}/contracts?page=${params.pagination.current}`)
-      .then((res) => {
-        const { data } = mapApiToProps(res.data);
-        this.setState({
-          loading: false,
-          data: data,
-          pagination: {
-            ...this.state.pagination,
-            ...res.data.pagination
-          }
-        })
-      })
-  };
-
-  render() {
-    const { data, pagination, loading } = this.state;
-
-    return (
-      <Table
-        columns={columns}
-        rowKey={(record) => record.id}
-        dataSource={data}
-        pagination={pagination}
-        loading={loading}
-        onChange={this.handleTableChange}
-      />
-    );
   }
+
+  return (
+    <Table
+      columns={columns}
+      rowKey={(record) => record.id}
+      dataSource={data}
+      pagination={pagination}
+      loading={loading}
+      onChange={handleTableChange}
+    />
+  );
 }
