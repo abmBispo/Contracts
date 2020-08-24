8b71ace7fae445d5e36041c110206aa832ed1276
Alan Borges <sr.alan.bispo@gmail.com>
2020-08-24 02:08:54 -0300

    frontend fixes

--- contracts-frontend/src/components/Part/Form/index.js
+++ contracts-frontend/src/components/Part/Form/index.js
@@ -32,24 +32,32 @@ const parseValues = (values) => {
   }
 }
 
+const requiredFields = [{ required: true }]
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is invalid!',
+  }
+};
+
+
 export default ({ match }) => {
   const [form] = Form.useForm();
+  const [user, setUser] = useState({});
   let history = useHistory();
 
-  const { partId } = match.params;
+  const { userId } = match.params;
 
-  const [initialValues, setInitialValues] = useState({});
-
-  useEffect(() => {
-    axios.get(`${BASE_URL}/parties/${partId}`)
-    .then((res) => {
-      const { data } = res.data;
-      form.setFieldsValue(data);
-      setInitialValues(data);
-    });
-  }, []);
-
-  console.log(initialValues);
+  if (match.params.length > 0) {
+    useEffect(() => {
+      axios.get(`${BASE_URL}/parties/${userId}`)
+        .then((res) => {
+          const { data } = res.data;
+          form.setFieldsValue(data);
+          setUser(data);
+        });
+    }, []);
+  }
 
   const create = (values) => {
     const parsedValues = parseValues(values);
@@ -62,30 +70,22 @@ export default ({ match }) => {
 
   const update = (values) => {
     const parsedValues = parseValues(values);
-    axios.put(`${BASE_URL}/parties/${partId}`, parsedValues).then(() => {
+    axios.put(`${BASE_URL}/parties/${userId}`, parsedValues).then(() => {
       form.resetFields();
       openMessage('Part has been updated successfully!');
       history.push('/parties');
     })
   }
 
-  const requiredFields = [{ required: true }]
-  const validateMessages = {
-    required: '${label} is required!',
-    types: {
-      email: '${label} is invalid!',
-    }
-  };
-
   return (
     <>
       <Breadcrumb style={{ margin: '16px 0' }}>
         <Breadcrumb.Item>App</Breadcrumb.Item>
         <Breadcrumb.Item href='/#/parties'>Parties</Breadcrumb.Item>
-        <Breadcrumb.Item>{initialValues ? `${initialValues.name} ${initialValues.surname}` : 'New Part'}</Breadcrumb.Item>
+        <Breadcrumb.Item>{userId ? `${user.name} ${user.surname}` : 'New Part'}</Breadcrumb.Item>
       </Breadcrumb>
-      <div className="site-layout-content">
 
+      <div className="site-layout-content">
         <Form
           form={form}
           labelCol={{ span: 4 }}
@@ -93,10 +93,9 @@ export default ({ match }) => {
           layout="horizontal"
           name='contract'
           size='large'
-          initialValues={initialValues}
-          onFinish={initialValues ? update : create}
+          onFinish={userId ? update : create}
           validateMessages={validateMessages}>
-            {}
+          {}
           <Form.Item rules={requiredFields} label="Name" name='name' required={false}>
             <Input />
           </Form.Item>
@@ -119,12 +118,10 @@ export default ({ match }) => {
 
           <Form.Item wrapperCol={{ span: 24 }}>
             <Button type="primary" htmlType="submit" style={{ float: 'right' }}>
-            {initialValues ? "Update" : "Save"}
+              {user ? "Update" : "Save"}
             </Button>
           </Form.Item>
-
         </Form>
-
       </div>
     </>
   );
